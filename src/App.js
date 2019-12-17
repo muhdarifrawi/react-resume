16d1e67a1bc4b34b556e83d96310645c7f5d1717
Arif <arifrawi@yahoo.com.sg>
2019-12-17 14:39:59 +0800

    experimented with rotating logo and full-length hero banner.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg'
 
 import 'bulma/css/bulma.css'
 
 function App() {
   return (
     <div className="App">
-      <div class="columns">
-        <div class="column">1
-          <button class="button">
-            Button
-          </button>
+      <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+        <figure class="image" >
+          <img className="App-logo" src={logo}/>
+        </figure>
+          <div class="container">
+            <h1 class="title">
+              Fullheight title
+            </h1>
+            <h2 class="subtitle">
+              Fullheight subtitle
+            </h2>
+          </div>
         </div>
-        <div class="column">2
-          <button class="button is-primary">
-            Primary button
-          </button>
-        </div>
-        <div class="column">3
-          <button class="button is-large">
-            Large button
-          </button>
-        </div>
-        <div class="column">4
-          <button class="button is-loading">
-            Loading button
-          </button>
-        </div>
-        <div class="column">5</div>
-      </div>
+      </section>
     </div>
   );
 }
