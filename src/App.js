eeeca4706bf51618b27c5ec1d2f9e11a2ad28113
Arif <arifrawi@yahoo.com.sg>
2019-12-17 14:28:04 +0800

    fixed importing code for Bulma.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import './App.css';
 
-import "bulma/sass/utilities/_all.sass"
-import "bulma/sass/grid/columns.sass"
+import 'bulma/css/bulma.css'
 
 function App() {
   return (
     <div className="App">
       <div class="columns">
-        <div class="column">1</div>
-        <div class="column">2</div>
-        <div class="column">3</div>
-        <div class="column">4</div>
+        <div class="column">1
+          <button class="button">
+            Button
+          </button>
+        </div>
+        <div class="column">2
+          <button class="button is-primary">
+            Primary button
+          </button>
+        </div>
+        <div class="column">3
+          <button class="button is-large">
+            Large button
+          </button>
+        </div>
+        <div class="column">4
+          <button class="button is-loading">
+            Loading button
+          </button>
+        </div>
         <div class="column">5</div>
       </div>
     </div>
