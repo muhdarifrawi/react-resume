0164d6351e6d13b0d4392dde24b8cc0be4be374a
Arif <arifrawi@yahoo.com.sg>
2020-01-06 16:22:44 +0800

    neatened up education section. Made content area to have fixed height.

--- src/Education.js
+++ src/Education.js
@@ -3,6 +3,31 @@ import './App.css';
 
 export default class Education extends React.Component {
 
+    state = {
+        title:"",
+        content:""
+    }
+    displayEducation = (event) => {
+        if (event.target.id == "secondarySch"){
+            this.setState({
+                title:"Secondary School",
+                content:""
+            })
+        }
+        else if (event.target.id == "poly"){
+            this.setState({
+                title:"Polytechnic",
+                content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet libero ac orci bibendum, at hendrerit augue interdum. Integer posuere, ligula euismod cursus luctus, enim tortor elementum lectus, id cursus nunc urna sed purus. Nunc suscipit volutpat efficitur. In hac habitasse platea dictumst. Morbi nec neque nunc. Vivamus nulla nunc, sollicitudin nec semper quis, rutrum dignissim purus. Maecenas enim tellus, ultrices id nisi id, porttitor tincidunt risus. Phasellus lobortis, sapien id aliquet eleifend, metus quam rhoncus libero, vel pellentesque nunc diam ut dui."
+            })
+        }
+        else{
+            this.setState({
+                title:"Bootcamp",
+                content:"Lorem ispum till the end of the world."
+            })
+        }
+    }
+
     render (){
     return(   
         <div> 
@@ -11,20 +36,21 @@ export default class Education extends React.Component {
                 <div class="tile is-vertical">
                 <div class="tile title is-parent">Education</div>
                 <div class="tile title">
-                    <div class="tile is-parent ">
-                        <div class="tile is-child box">Trent Global College</div>
+                    <div class="tile is-parent">
+                        <div class="tile is-child box" id="bootcamp" onMouseOver={this.displayEducation}>Trent Global College</div>
                     </div>
-                    <div class="tile is-parent ">
-                        <div class="tile is-child box">Singapore Polytechnic</div>
+                    <div class="tile is-parent">
+                        <div class="tile is-child box"  id="poly" onMouseOver={this.displayEducation}>Singapore Polytechnic</div>
                     </div>
-                    <div class="tile is-parent ">
-                        <div class="tile is-child box">Changkat Changi Secondary</div>
+                    <div class="tile is-parent">
+                        <div class="tile is-child box"  id="secondarySch" onMouseOver={this.displayEducation}>Changkat Changi Secondary</div>
                     </div>
                 </div>
                 <div class="tile">
                     <div class="tile is-parent">
-                        <div class="tile is-child box">
-                            Wololol
+                        <div class="tile is-child box customBox1">
+                            <p>{this.state.title}</p>
+                            <p>{this.state.content}</p>
                         </div>
                     </div>
                 </div>
