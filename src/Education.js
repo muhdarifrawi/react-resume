1ecdb2b97fcda6ba93e8058e544ca5511215dd95
Arif <arifrawi@yahoo.com.sg>
2020-01-13 12:00:18 +0800

    Edited content for Language and Education display.

--- src/Education.js
+++ src/Education.js
@@ -11,19 +11,36 @@ export default class Education extends React.Component {
         if (event.target.id == "secondarySch"){
             this.setState({
                 title:"Secondary School",
-                content:""
+                content:`Changkat Changi is a neighbourhood school located in the eastern side of Singapore. 
+                They pride in having students excelling in both studies and co-curriculum activities. One of 
+                the school's forte is their knowledge in technical studies. The shcool's Design and Tech curriculum 
+                has been the most consistent and well-known to be the best within secondary schools.  
+                `
             })
         }
         else if (event.target.id == "poly"){
             this.setState({
                 title:"Polytechnic",
-                content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet libero ac orci bibendum, at hendrerit augue interdum. Integer posuere, ligula euismod cursus luctus, enim tortor elementum lectus, id cursus nunc urna sed purus. Nunc suscipit volutpat efficitur. In hac habitasse platea dictumst. Morbi nec neque nunc. Vivamus nulla nunc, sollicitudin nec semper quis, rutrum dignissim purus. Maecenas enim tellus, ultrices id nisi id, porttitor tincidunt risus. Phasellus lobortis, sapien id aliquet eleifend, metus quam rhoncus libero, vel pellentesque nunc diam ut dui."
+                content:`
+                A graduate in Diploma in Clean Energy. Contrary to what many would think, this is 
+                actually an Electrical and Electronic Engineering course. Students are primarily taught
+                on how power grid systems function and how clean energy systems can be linked up to them.
+                Majority of the lessons consists of Photovoltaic systems but various systems were taught 
+                as well. Such as wind turbine, hydroelectric, fuel cells and biodiesels.
+                `
             })
         }
         else{
             this.setState({
-                title:"Bootcamp",
-                content:"Lorem ispum till the end of the world."
+                title:"Code Institute, Bootcamp",
+                content:`
+                A  four months Bootcamp endorsed by Info-communications Media Development Authority (IMDA).
+                Targeted to working adults thinking of a career change to a tech industry. During these 
+                four months, students are taught on how to develop a full-stack webpage. They are separated
+                into four parts, with each part having new languages taught to us. Students are required to 
+                submit projects each month. Students graduate of Trent Global Bootcamp graduates with a 
+                Diploma in Software Development. 
+                `
             })
         }
     }
@@ -49,7 +66,7 @@ export default class Education extends React.Component {
                 <div class="tile">
                     <div class="tile is-parent">
                         <div class="tile is-child box customBox1">
-                            <p>{this.state.title}</p>
+                            <p class="title">{this.state.title}</p>
                             <p>{this.state.content}</p>
                         </div>
                     </div>
--- src/Languages.js
+++ src/Languages.js
@@ -3,29 +3,46 @@ import './App.css';
 
 export default class Language extends React.Component {
     state = {
-        title:""
+        title:"",
+        frameworks: "",
+        content:""
     }
 
     displayLanguages = (event) => {
         console.log(event.target.id)
         if (event.target.id == "one"){
             this.setState({
-                title: "HTML",
-                content: "lorem ipsum"
+                title: "HTML/CSS",
+                frameworks: `Frameworks: Bootstrap, Materialize, Bulma`,
+                content: `
+                The basic knowledge of most developers. This is where most of us starts in. 
+                Knowing how your website would look like and seeing how each code does to each
+                component. 
+                `
             })
         }
 
         else if (event.target.id == "two") {
             this.setState({
                 title:"JavaScript",
-                content: "kawabunga"
+                frameworks: `Frameworks: JQuery`,
+                content: `Used heavily for DOM manipulation and using AXIOS for API applications. 
+                Having knowledge of JavaScript is useful in creating more dynamic webpages
+                and implementing API to your webpage. 
+                `
             })
         }
 
         else{
             this.setState({
                 title:"Python",
-                content:"if you were here"
+                frameworks: `Frameworks: Flask, Django`,
+                content:`Used as an intermediary code to communicate between webpage and database servers.
+                Flask allows users to navigate to other pages with a dedicated URL. With more practice, users
+                of Flask would be able to create a fully fleshed e-commerce platform. Django however, is a more 
+                complete framework. Users of Django would not have to worry much on the design aspect as it is 
+                built for 'plug and play'.
+                `
             })
         }
     }
@@ -33,7 +50,7 @@ export default class Language extends React.Component {
     render (){
     return(    
         <div>
-            <div class="tile is-ancestor box" className="background">
+            <div class="tile is-ancestor box">
                 <div class="tile is-vertical">
                     <div class="tile is-parent title">Languages</div>
                         <div class="tile">
@@ -54,6 +71,7 @@ export default class Language extends React.Component {
                             <div class="tile is-parent">
                                 <div class="tile is-child box">
                                 <p class="title">{this.state.title}</p>
+                                <p class="subtitle">{this.state.frameworks}</p>
                                 <p>{this.state.content}</p>
                                 </div>
                             </div>
